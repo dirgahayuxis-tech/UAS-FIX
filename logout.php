66adfbbd3a7d1038cc8dea5218f445ff058d5f6c
Dirgahayu <dirgahayuxis@gmail.com>
2026-07-08 09:40:23 +0800

    perbaharuan arah

--- logout.php
+++ logout.php
@@ -1,5 +1,5 @@
 <?php
 session_start();
 session_destroy(); // Menghapus data login
-echo "<script>alert('Anda telah berhasil logout!'); window.location='login.php';</script>";
+echo "<script>alert('Anda telah berhasil logout!'); window.location='landing-page.php';</script>";
 ?>
