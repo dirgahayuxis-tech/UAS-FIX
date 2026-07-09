a5250e10f1879cb7721f35be1bb000f79334775a
Dirgahayu <dirgahayuxis@gmail.com>
2026-07-09 10:22:29 +0800

    kesekian kalinnnya

--- koneksi.php
+++ koneksi.php
@@ -1,15 +1,13 @@
 <?php
 $host = "localhost";
-$port = "5432"; 
+$port = "5432";
 $dbname = "db_laundry";
-$user = "postgres"; 
-$password = "211020"; 
+$user = "postgres";
+$password = "211020";
 
-$string_koneksi = "host=$host port=$port dbname=$dbname user=$user password=$password";
-$koneksi = pg_connect($string_koneksi);
+$conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
 
-if (!$koneksi) {
-    // Menambahkan $koneksi ke dalam parameter untuk mengatasi peringatan VS Code
-    die("Koneksi gagal: " . pg_last_error($koneksi));
+if (!$conn) {
+    die("Koneksi PostgreSQL gagal.");
 }
 ?>
