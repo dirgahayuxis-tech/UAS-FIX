95d1cb74548e3f75a95138d9f4de5b6ada8a8eed
Dirgahayu <dirgahayuxis@gmail.com>
2026-07-09 10:46:31 +0800

    kesekian kalia

--- koneksi.php
+++ koneksi.php
@@ -1,15 +1,29 @@
 <?php
-$host = "localhost";
-$port = "5432";
-$dbname = "db_laundry";
-$user = "postgres";
-$password = "ISI_PASSWORD_POSTGRES_ANDA";
+$database_url = getenv("DATABASE_URL");
 
-$string_koneksi = "host=$host port=$port dbname=$dbname user=$user password=$password";
+if ($database_url) {
+    // Koneksi untuk hosting online
+    $url = parse_url($database_url);
 
-$koneksi = pg_connect($string_koneksi);
+    $host = $url["host"];
+    $port = $url["port"] ?? 5432;
+    $dbname = ltrim($url["path"], "/");
+    $user = $url["user"];
+    $password = $url["pass"];
+
+    $koneksi = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password sslmode=require");
+} else {
+    // Koneksi untuk laptop lokal
+    $host = "localhost";
+    $port = "5432";
+    $dbname = "db_laundry";
+    $user = "postgres";
+    $password = "211020";
+
+    $koneksi = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
+}
 
 if (!$koneksi) {
-    die("Koneksi gagal ke database PostgreSQL.");
+    die("Koneksi PostgreSQL gagal.");
 }
 ?>
