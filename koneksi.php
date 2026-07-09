ef5de6eeaf09733af3d62814a6ae2fb70f370597
Dirgahayu <dirgahayuxis@gmail.com>
2026-07-09 10:40:36 +0800

    kesekian kalia

--- koneksi.php
+++ koneksi.php
@@ -3,11 +3,13 @@ $host = "localhost";
 $port = "5432";
 $dbname = "db_laundry";
 $user = "postgres";
-$password = "211020";
+$password = "ISI_PASSWORD_POSTGRES_ANDA";
 
-$conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
+$string_koneksi = "host=$host port=$port dbname=$dbname user=$user password=$password";
 
-if (!$conn) {
-    die("Koneksi PostgreSQL gagal.");
+$koneksi = pg_connect($string_koneksi);
+
+if (!$koneksi) {
+    die("Koneksi gagal ke database PostgreSQL.");
 }
 ?>
